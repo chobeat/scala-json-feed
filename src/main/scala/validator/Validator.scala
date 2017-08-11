51d59110f01133cb569bb1047bae704615177b08
chobeat <simone.robutti@gmail.com>
2017-08-11 17:24:02 +0200

    Added Item schema first version

--- src/main/scala/validator/Validator.scala
+++ src/main/scala/validator/Validator.scala
@@ -1,5 +1,10 @@
 package validator
-import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
+import cats.data.ValidatedNel
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
+
 import scala.util.{Success, Try}
 
 /**
@@ -7,11 +12,36 @@ import scala.util.{Success, Try}
   */
 
 case class JSONFeedDocument(content:Option[String])
+
+case class JSONFeedAuthor(name:Option[String],
+                          url:Option[String],
+                          avatar:Option[String])
+
+case class JSONFeedItem(id:String,
+                        url:Option[String]=None,
+                        external_url:Option[String]=None,
+                        title:Option[String]=None,
+                        content_html:Option[String]=None,
+                        content_text:Option[String]=None,
+                        image: Option[String]=None,
+                        banner_image:Option[String]=None,
+                        date_published:Option[String]=None,
+                        date_modified: Option[String]=None,
+                        author:Option[JSONFeedAuthor]=None,
+                        tags:Option[List[String]]=None
+                       )
+
 object Validator {
 
 
-  def validate(s:String): Either[Error, JSONFeedDocument] ={
-    decode[JSONFeedDocument](s)
+  def validate(s:String): ValidatedNel[Error, JSONFeedDocument] ={
+    decodeAccumulating[JSONFeedDocument](s)
 
   }
+
+  def validateItem(s:String): ValidatedNel[Error, JSONFeedItem] ={
+    decodeAccumulating[JSONFeedItem](s)
+
+  }
+
 }
--- src/test/scala/validator/ValidatorSpec.scala
+++ src/test/scala/validator/ValidatorSpec.scala
@@ -1,12 +1,34 @@
 package validator
 
+import cats.data.{NonEmptyList, Validated}
+import cats.data.Validated.Invalid
+import io.circe.CursorOp.DownField
+import io.circe.DecodingFailure
 import org.scalatest._
 
-import scala.util.Success
+
 
 class ValidatorSpec extends FlatSpec with Matchers {
-  "The Validator object" should "return a Success(JValue)" in {
+  def parsingError(fields: String*)=Validated.invalidNel(
+    DecodingFailure("Attempt to decode value on failed cursor", fields.map(DownField).toList))
+
+  "The validate function" should "return an empty JSONFeedDocument" in {
     val input = "{}"
-  Validator.validate(input) shouldEqual Right(JSONFeedDocument(None))
+    Validator.validate(input) shouldEqual Right(JSONFeedDocument(None))
+  }
+
+  "The validateItem function" should "return a JSONFeedItem" in {
+    val input = """{"id":"abc","tags":["a","b","c"]}"""
+
+    Validator.validateItem(input) shouldEqual Right(
+      JSONFeedItem("abc", tags = Some(List("a", "b", "c"))))
+
+  }
+
+  it should "return an error if the id is missing" in {
+    val input = """{"tags":["a","b","c"]}"""
+
+    Validator.validateItem(input) shouldEqual parsingError("id")
+
   }
 }
